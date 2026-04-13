cb5d38eed5eee6a2638ed38fce9389320d021de1
fiqihbadrian <fiqihbadrian@gmail.com>
2026-04-14 00:29:11 +0700

    webpro

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,17 +1,3 @@
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="<?= base_url('public/assets/css/style.css') ?>">
-	<title>Document</title>
-</head>
-<body>
-	<nav>
-		<ul>
-			<li><a href="<?= base_url('index.php') ?>">Home</a></li>
-			<li><a href="<?= base_url('index.php/profile') ?>">Profile</a></li>
-		</ul>
-	</nav>
-	<h1>Halo ini halaman home</h1>
-</body>
-</html>
+<li><a href="<?= base_url('index.php') ?>">Home</a></li>
+<li><a href="<?= base_url('index.php/profile') ?>">Profile</a></li>
+<h1>Halo ini halaman home</h1>
